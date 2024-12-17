ae9c72978df142225cc1fbc39938d074913dd05a
Ciobanu Catalin <60657271+dredd746@users.noreply.github.com>
2024-12-17 17:05:24 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,15 @@ import { ConfigService } from '@nestjs/config';
 import { RolesGuard } from './modules/auth/guards/roles.guard';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: {
-      origin: "*",
-      credentials: true,
-    },
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: true, // Allows all origins (set to "*" explicitly if needed)
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true, // Allow cookies and credentials
   });
+
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
